f654e44362899b3decd69c29fdbd547aefd48824
Tamtikorn <gan_zaza_12@hotmail.com>
2024-07-25 21:58:01 +0700

    Refactor asteroid_collision function for improved readability and performance

--- lab-6/ood-5.py
+++ lab-6/ood-5.py
@@ -1,31 +1,21 @@
-def is_left(asts, current):
-    if asts[current] < 0:
-        return True
-    return False
-
-def is_right(asts, current):
-    if asts[current] > 0:
-        return True
-    return False
-
-def is_collision(asts, i, is_left):
-    pass
-
-
 def asteroid_collision(asts: list, i = 0):
-    if i == 0 and is_left(asts, i):
-        return asteroid_collision(asts, i+1)
-    if i == len(asts) - 1:
-        if is_left(asts, i):
-            is_collision(asts, i)
+    if i >= len(asts) - 1:
         return asts
-    cond = is_left(asts, i)
-    if cond:
-        lst = asts[:i]
-    else:
-        lst = asts[i+1:]
-    is_collision(lst, i, cond)
-    return asteroid_collision(asts, i+1)
+    cur = asts[i]
+    next = asts[i + 1] if i + 1 < len(asts) else None
+    if next is not None:
+        if (next < 0 and cur > 0) and abs(cur) == abs(next):
+            asts.pop(i)
+            asts.pop(i)
+            return asteroid_collision(asts, 0)
+        elif cur > 0 and next < 0 and abs(cur) > abs(next):
+            asts.pop(i + 1)
+            return asteroid_collision(asts, 0)
+        elif cur > 0 and next < 0 and abs(cur) < abs(next):
+            asts.pop(i)
+            return asteroid_collision(asts, 0)
+        return asteroid_collision(asts, i + 1)
+    return asts
 
 x = input("Enter Input : ").split(",")
 x = list(map(int,x))
